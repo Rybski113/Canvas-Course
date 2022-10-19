56e2de11abeca8e9da683a93059d9b0636c4b404
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-10-19 16:55:50 +0200

    setup and resize canvas

---
+++ app.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById('canvas1'); 
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+window.addEventListener('resize', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.fillStyle = 'white';
+    ctx.fillRect(10, 20, 150, 50);
+})
+
+ctx.fillStyle = 'white';
+ctx.fillRect(10, 20, 150, 50);
