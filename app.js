41eb3ee49987e6c3b590c467736a1cfa5953e7ee
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-10-19 19:57:26 +0200

    particles removed
    after they completely shrink, they are being removed from Array

--- app.js
+++ app.js
@@ -31,18 +31,19 @@ class Particle {
        // this.y = mouse.y;
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 5 + 1;
+        this.size = Math.random() * 25 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
     }
     update() {
         this.x += this.speedX;
         this.y += this.speedY;
+        if (this.size > 0.2) this.size -= 0.1;
     }
     draw() {
         ctx.fillStyle = 'blue';
         ctx.beginPath();
-        ctx.arc(this.x, this.y , 50, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y , this.size, 0, Math.PI * 2);
         ctx.fill()
     }
 }
@@ -58,6 +59,11 @@ function handleParticles() {
     for (let i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update();
         particlesArray[i].draw();
+        if (particlesArray[i].size <= 0.3) {
+            particlesArray.splice(i, 1);
+            console.log(particlesArray.length);
+            i--;
+        }
     }
 }
 
@@ -68,6 +74,7 @@ function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     handleParticles()
     requestAnimationFrame(animate);
+    
 }
 animate();
 
